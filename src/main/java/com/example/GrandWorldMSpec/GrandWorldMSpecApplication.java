78fe7a343a1d1735763a84d39c07c5e353ae9c7d
zhaoheting <492995407@qq.com>
2020-02-15 22:01:45 +0800

    Init a spec project.

---
+++ src/main/java/com/example/GrandWorldMSpec/GrandWorldMSpecApplication.java
@@ -0,0 +1,13 @@
+package com.example.GrandWorldMSpec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandWorldMSpecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandWorldMSpecApplication.class, args);
+	}
+
+}
