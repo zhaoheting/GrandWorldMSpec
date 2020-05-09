262f0a8711d564967cd883250aa3f3a253df03ed
zhaoheting <492995407@qq.com>
2020-05-09 12:24:33 +0800

    Code base.

--- src/main/java/com/example/GrandWorldMSpec/swagger/SwaggerDocumentationConfig.java
+++ src/main/java/com/example/GrandWorldMSpec/swagger/SwaggerDocumentationConfig.java
@@ -10,7 +10,7 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-03-18T14:03:57.953+08:00")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-05-09T12:21:42.864+08:00")
 
 @Configuration
 public class SwaggerDocumentationConfig {
